4d899795f7e6f65daf08e10b64893a4365ff2dac
Robert Byaruhanga <byaruhangarobert062@gmail.com>
2026-05-20 23:24:17 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,14 @@
 
 <?php
+host = "localhost";
+$user = "root";
+$password = "";
+$database = "eventflow";
 
-//Pretty add code here and remove this comment
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 ?>
